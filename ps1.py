f12e7712b2243634f9e3359088521a9b3576d903
evelynchen1229 <evelynchen1229@gmail.com>
2020-04-27 23:42:50 +0100

    ps1a

--- ps1.py
+++ ps1.py
@@ -1,2 +1,32 @@
+# part A - House Hunting: how long to save enough deposit
 import numpy
 import matplotlib
+
+total_cost = float(input("what's the cost of your dream house? "))
+portion_down_payment = 0.25
+deposit = total_cost * portion_down_payment
+annual_salary = float(input("what's your annual salary? "))
+portion_saved = float(input("how much % would you save? decimal number please: "))
+monthly_salary = annual_salary/12
+r = 0.04 #annual_retun
+
+month = 0
+current_saving = 0
+#should loop until current saving is enough for the deposit
+
+while current_saving < deposit:
+
+  current_saving = current_saving * (1 + r/12) + monthly_salary * portion_saved
+  #by the end oh month_0,the beginning of month_1
+
+  month += 1
+  if month > 200:
+    break
+
+print (month)
+print(current_saving)
+#month = 12,
+
+
+
+
