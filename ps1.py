1db0a150a8c3c1084af1de2761db16de32b82e0c
evelynchen1229 <evelynchen1229@gmail.com>
2020-04-29 23:25:05 +0100

    ps1c semi done needs debuggin

--- ps1.py
+++ ps1.py
@@ -59,49 +59,9 @@
 ##print(current_saving)
 #
 ##part c: finding the right amount to save away bisection search
-#semi_annual_raise = .07
-#r = .04
-#total_cost = 1000000
-#deposit = total_cost * .25
-#annual_salary = float(input("what's your annual salary? "))
-#monthly_salary = annual_salary / 12
-#
-#0 <= portion_saved <= 10000 #integer
-#percentage = portion_saved / 10000 #four decimals
-#
-#months = 36
-#diff = 100
-#
-#current_saving = 0
-#
-#def bisection(guess, target):
-#  if guess == target:
-#    print("guess")
-#  elif guess < target:
-#    # run the function to increase the guess
-#    guess = (10000+guess)/2
-#    bisection(guess2, target)
-#  elif guess > target:
-#    #run the function to lower the guess
-#    guess = (0+guess)/2
-#    bisection(guess,target)
-#step = 0
-#  month = 0
-#  current_saving = 0
-#    if month % 6 == 0 and month >0:
-#      monthly_salary = monthly_salary * (1 + semi_annual_raise)
-#
-#    current_saving = current_saving * (1 + r/12) + monthly_salary * int(portion_saved)/10000
-#    #by the end oh month_0,the beginning of month_1
-#
-#    month += 1
-#    if month > 36:
-#      break
-#  bisection(current_saving,target)
-#def current_saving (monthly_salary,portion_saved):
-#  if
-#
-def current_saving(saving_portion=100):
+
+def comparison(saving_portion): # return the different between deposit - current_saving
+#get current saving in 3 years
   month = 0
   deposit = 1000000*.25
   monthly_salary = 150000/12
@@ -113,39 +73,57 @@ def current_saving(saving_portion=100):
     month += 1
   #    print(month)
    #   print(monthly_salary)
-    if month > 37:
+    if month > 35:
       break
-  return current_saving
+  diff = int(current_saving - deposit)
+#  print(diff)
+ # print(current_saving)
+  #print(month)
+  #print(saving_portion)
 
-saving = current_saving()
-deposit = 1000000*.25
-step = 0
-saving_percentage = 100
-while saving < deposit - 100:
-  saving_percentage = int((saving_percentage + 10000)/2)
-  saving = current_saving(saving_percentage)
+  return diff
 
-  step +=1
 
-  if step > 20:
-    break
+def saving_rate(saving_per = 5000, step=1,high=10000,low=0):
+  compare_point = comparison(saving_per)
+#  print('compare_point', compare_point)
 
-print("step: ",step)
-print("saving: ",saving)
-print("saving portion: ",saving_percentage/10000)
+  if compare_point >= 0 and compare_point <= 100:
+    result = int(saving_per)
+    return {"saving portionl": result, "steps": step}
 
-month = 0
-deposit = 1000000*.25
-monthly_salary = 150000/12
-current_saving = 0
-while current_saving < deposit:
-  if month % 6 == 0 and month >0:
-    monthly_salary = monthly_salary* (1 + .07)
-  current_saving = current_saving * (1 + .04/12) + monthly_salary * 4411/10000
-  month += 1
-  #    print(month)
-   #   print(monthly_salary)
-  if month > 37:
-    break
-print(month)
-print(current_saving)
+  if compare_point > 10:
+    new_high = saving_per
+    new_saving_per = (low + high) / 2
+
+    return saving_rate(new_saving_per, step + 1, new_high, low)
+
+  new_low = saving_per
+  new_saving_per = (low + high) / 2
+
+  return saving_rate(new_saving_per, step + 1, high, new_low)
+
+
+a = saving_rate()
+print(a)
+
+#the codes below returns "None" because of parameter names being mutated for the new values
+# should assign new variable names if the diffult value has changed
+
+#if 0 <= compare_point and compare_point <= 100:
+  #  result=int(saving_per)
+  #  print('saving_per', result)
+  #  return str(result)
+  #elif compare_point > 10: #saving rate being too high
+  #  high = saving_per
+  #  saving_per = (low+high)/2
+  #  step +=1
+  #  saving_rate(saving_per,step,high,low)
+  #else: # saving rat being too low
+  #  low = saving_per
+  #  saving_per = (low+high)/2
+  #  step +=1
+  #  saving_rate(saving_per,step,high,low)
+
+
+
