ddf2db84e056ca028266dd3b966856732b308283
evelynchen1229 <evelynchen1229@gmail.com>
2020-05-04 23:10:45 +0100

    attempt on the main body. uptill testing the situation whether the input is alpha/guessed already/and in the secret word. need to fix 1. dynamic available letters (not excluding the guessed one atm). 2. dynamic word_now (one letter only is being shown now)

--- hangman.py
+++ hangman.py
@@ -83,7 +83,7 @@ def is_word_guessed(secret_word, letters_guessed):
 #is_guessing = is_word_guessed('apple',is_guessed)
 
 
-def get_guessed_word(secret_word, letters_guessed):
+def get_guessed_word(secret_word, letters_guessed=''):
     '''
     secret_word: string, the word the user is guessing
     letters_guessed: list (of letters), which letters have been guessed so far
@@ -94,15 +94,18 @@ def get_guessed_word(secret_word, letters_guessed):
     secret_letters = list(secret_word)
     print(secret_letters)
     guessed_letters = []
-    for i in secret_letters:
-        if i in letters_guessed:
-            guessed_letters.append(i)
-        else:
-            guessed_letters.append('_ ')
-    result ="".join(guessed_letters)
-    print("'",result,"'")
+    if len(letters_guessed) >0:
+        for i in secret_letters:
+            if i in letters_guessed:
+                guessed_letters.append(i)
+            else:
+                guessed_letters.append('_ ')
+        result ="".join(guessed_letters)
+    else:
+        result = '-'*len(secret_word)
+#    print(result)
     return result
-    pass
+ #   pass
 #guessing_word = input("guess a word please: ").lower()
 ##assert guessing_word.islower(), 'please put lower case word'
 #guessed_word = list(guessing_word)
@@ -111,30 +114,35 @@ def get_guessed_word(secret_word, letters_guessed):
 
 
 
-def get_available_letters(letters_guessed):
+def get_available_letters(letters_guessed=''):
     '''
     letters_guessed: list (of letters), which letters have been guessed so far
     returns: string (of letters), comprised of letters that represents which letters have not
       yet been guessed.
     '''
     # FILL IN YOUR CODE HERE AND DELETE "pass"
+    full_letters = string.ascii_lowercase
     full_lowercase_list = list(string.ascii_lowercase)
     all_available_letters = []
-    for i in full_lowercase_list:
-        if i in letters_guessed:
-            pass
-        else:
-            all_available_letters.append(i)
+    if len(letters_guessed)>0:
 
-    result = "".join(all_available_letters)
+        for i in full_lowercase_list:
+            if i in letters_guessed:
+                pass
+            else:
+                all_available_letters.append(i)
+
+        result = "".join(all_available_letters)
+    else:
+        result = full_letters
     print(result)
     return result
 
-guessing_word = input("guess a word please: ").lower()
+#guessing_word = input("guess a word please: ").lower()
 #assert guessing_word.islower(), 'please put lower case word'
-guessed_available = list(guessing_word)
+##guessed_available = list(guessing_word)
 
-available_letters = get_available_letters(guessed_available)
+#available_letters = get_available_letters(guessed_available)
 
 
 def hangman(secret_word):
@@ -162,9 +170,74 @@ def hangman(secret_word):
 
     Follows the other limitations detailed in the problem write-up.
     '''
-    # FILL IN YOUR CODE HERE AND DELETE "pass"
-    pass
+    secrect_len = len(secret_word)
+    warnings = 3
+    guessing_round = 6
+    deducting = 0
+    print('''
+    Welcom to the game Hangman!
+    I am thinking of a word that is {0} letters long.
+    You have {1} warnings left.
+    ------------------------'''.format(secrect_len,warnings)
+    )
+    guessed_list = []
+    while guessing_round >0:
+        remaining_guessing = guessing_round - deducting
+        available_letters = get_available_letters()
 
+        print('''
+        You have {0} guesses left.
+        Available letters: {1}
+        '''.format(remaining_guessing,available_letters)
+        )
+        letter_guessed = input("Please guess a letter: ")
+
+        if letter_guessed.isalpha():
+            if letter_guessed in guessed_list:
+                if warnings == 0:
+                    guessing_round -= 1
+                    print('''You've guessed this letter already. Remaining
+                    guessing chances: ''', guessing_round)
+                else:
+                    warnings -= 1
+                print("You've guessed this letter already, Remaining guessing chances: ",
+                        guessing_round)
+            elif letter_guessed in secret_word :
+                guessed_list.append(letter_guessed)
+                word_now = get_guessed_word(secret_word, letter_guessed)
+                print("good guess. the current word is: ",word_now, " remaining guessing",
+                        guessing_round)
+
+                new_available_letters = get_available_letters()
+
+
+                #if in the secret word
+                #elif not in the secret, and is constant: guessing -1
+                #else: guessing -2
+            #else
+                #warning warning -1; warning  = 0 to start with, guessing -1
+
+        elif warnings == 0:
+            guessing_round -= 1
+            print('''Please enter an letter. You have no warnings left and lost one guessing round.
+            Remaining guessing chances: ''', guessing_round)
+        else:
+            warnings -= 1
+            print('Oops! That is not a valid letter. You have ',warnings,' warnings left')
+            print('Remaining guessing rounds: ',guessing_round)
+    if word_now == secret_word:
+        print("you win")
+        return word_now
+    else:
+        print("you lost")
+        return word_now
+
+
+
+    # FILL IN YOUR CODE HERE AND DELETE "pass"
+#    pass
+test = hangman('apple')
+print(test)
 
 
 # When you've completed your hangman function, scroll down to the bottom
@@ -244,15 +317,15 @@ def hangman_with_hints(secret_word):
 # Hint: You might want to pick your own secret_word while you're testing.
 
 
-if __name__ == "__main__":
-    # pass
-
-    # To test part 2, comment out the pass line above and
-    # uncomment the following two lines.
-
-    secret_word = choose_word(wordlist)
-    hangman(secret_word)
-
+#if __name__ == "__main__":
+#    # pass
+#
+#    # To test part 2, comment out the pass line above and
+#    # uncomment the following two lines.
+#
+#    secret_word = choose_word(wordlist)
+#    hangman(secret_word)
+#
 ###############
 
     # To test part 3 re-comment out the above lines and
